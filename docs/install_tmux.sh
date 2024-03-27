b16f3c05be7fc75400f74a958307a72e78871b8e
kingscott <me@kingscott.ca>
2024-03-27 19:17:26 -0400

    Push new zips for nvim, tmux, alacritty + installers

---
+++ docs/install_tmux.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+
+# Setup base .config files
+if ! [ -d "~/.tmux/" ]; then
+    mdkir ~/.tmux/
+fi
+cd ~/.tmux/
+
+# Grab zip file of nvim configs from hosted site
+wget https://kingscott.github.io/.dotfiles/tmux.zip .
+unzip tmux.zip 
+rm tmux.zip 
+
+# Setup tmux conf
+mv ./tmux.conf ~/.tmux.conf
+
+# Copy files downloaded from repo to local config directory (~/.config)
+cp -r ./tmux/ ./ 
+
+# Open tmux to finish install:
+echo "Please open tmux to finish the install by entering Ctrl + I and Ctrl + U".
